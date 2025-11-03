b8fe09a924e64d8a9d6fd8a284dcced1ceae41da
Curro Portillo <211curroportillo@gmail.com>
2025-11-03 16:55:45 +0100

    ejercicio 31

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Introduzca la altura de la L: "));
+        for(int i = 1; i<altura; i++){
+            System.out.println("*");
+        }
+        for(int i = 0; i<(altura/2 + 1); i++){
+            System.out.print("* ");
+        }
     }
 }
